f1d6bf836ff068a14efd6ad91d88522d1c54319d
Waseem-khan7 <waseemnasirkhan24@gmail.com>
2025-07-29 00:43:48 +0530

    First Commit

---
+++ MongoDB-CRUD/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { connectMongoDb } = require("./connection");
+
+const { logReqRes } = require("./middlewares");
+
+const userRouter = require("./routes/user");
+
+const app = express();
+const PORT = 8000;
+
+//Connection
+connectMongoDb("mongodb://127.0.0.1:27017/app-1").then(() =>
+  console.log("MongoDB Connected")
+);
+
+//Middleware - (BuiltIn)
+app.use(express.urlencoded({ extended: false }));
+app.use(logReqRes("log.txt"));
+
+app.use("/api/users", userRouter);
+
+app.listen(PORT, () => console.log(`Server started at Port ${PORT}`));
